c0ec4e7f54c0158075e63ed45a83ceb6fe9dc363
sasirajpuvvada <sasirajluck@gmail.com>
2021-01-03 00:46:40 +0530

    MCM Intro

--- DP_MCM/Q0MCM.java
+++ DP_MCM/Q0MCM.java
@@ -2,6 +2,21 @@ package DP_MCM;
 
 public class Q0MCM {
     public static void main(String[] args) {
-        
+        int[] ar = {10,30,5,60};
+        int n = ar.length,l = 1,r = n - 1;
+        int ans = MCMrecusrion(ar,l,r);
+        System.out.print(ans);
+    }
+
+    public static int MCMrecusrion(int[] ar, int l, int r) {
+        if (l == r)
+            return 0;
+        int ans = Integer.MAX_VALUE;
+        for (int i = l; i < r; i++) {
+            int tempRes = MCMrecusrion(ar,l,i) + MCMrecusrion(ar, i+1, r) + (ar[l-1] * ar[i] * ar[r]);
+//            System.out.println(tempRes);
+            ans = Math.min(ans,tempRes);
+        }
+        return ans;
     }
 }
